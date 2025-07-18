e4d84dca19eaa3980ff3d34d3c3ae1d3baf87460
agungandhita <agungandhita@gmail.com>
2025-07-18 16:04:10 +0700

    refactor(database): comment out LayananSeeder in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             AdminSeeder::class,
-            LayananSeeder::class,
+            // LayananSeeder::class,
         ]);
     }
 }
